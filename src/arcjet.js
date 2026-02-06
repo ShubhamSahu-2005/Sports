fca42c3f04919f68f1035d9fff4c1aa19b31cfcb
ShubhamSahu-2005 <shubhamsahu5122333@gmail.com>
2026-02-06 17:12:13 +0530

    Added bot security and Rate Limiting using Arcjet

---
+++ src/arcjet.js
@@ -0,0 +1,51 @@
+import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/node";
+
+const arcjetKey = process.env.ARCJET_KEY;
+const arcjetMode = process.env.ARCJET_MODE === 'DRY_RUN' ? 'DRY_RUN' : 'LIVE';
+
+
+if (!arcjetKey) {
+    throw new Error("Arcjet Key is Missing");
+}
+export const httpArcjet = arcjetKey ? arcjet({
+    key: arcjetKey, rules: [
+        shield({ mode: arcjetMode }),
+        slidingWindow({ mode: arcjetMode, interval: '10s', max: 50 }),
+        detectBot({ mode: arcjetMode, allow: ['CATEGORY:SEARCH_ENGINE', 'CATEGORY:PREVIEW'] })
+    ]
+
+}) : null;
+
+export const wsArcjet = arcjetKey ? arcjet({
+    key: arcjetKey, rules: [
+        shield({ mode: arcjetMode }),
+        slidingWindow({ mode: arcjetMode, interval: '2s', max: 5 }),
+        detectBot({ mode: arcjetMode, allow: ['CATEGORY:SEARCH_ENGINE', 'CATEGORY:PREVIEW'] })
+    ]
+
+}) : null;
+
+export async function securityMiddleware(req, res, next) {
+    if (!httpArcjet) return next();
+
+    try {
+        const decision = await httpArcjet.protect(req);
+
+        if (decision.isDenied()) {
+
+            if (decision.reason?.isRateLimit?.()) {
+                return res.status(429).json({ error: "Too many requests" });
+            }
+
+            return res.status(403).json({ error: "Access Denied" });
+        }
+
+        return next();
+
+    } catch (error) {
+        console.error("Arcjet middleware error:", error);
+        return res.status(503).json({ error: "Service Unavailable" });
+    }
+}
+
+
